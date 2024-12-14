199168027f742ca20bb741ae1482450a537b545f
beeplovebhandari <bbhandari0304@gmail.com>
2024-12-14 21:43:49 +0545

    inheritance

---
+++ Day_21/1_inheritance.py
@@ -0,0 +1,8 @@
+# Inheritance is the transfer of proporties and methods to the child class from the parent class
+# Types of inheritance in pyhton;
+# 1. Single Inheritance
+# 1. Multiple Inheritance
+# 1. Multilevel Inheritance
+# 1. Hierarchical Inheritance
+# 1. Hybrid Inheritance
+
